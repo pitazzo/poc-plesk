93dd013129c5b6384d979c7d110ec0035c80ac7c
Pedro Malo <pedromalo@live.com>
2024-11-18 19:25:30 +0100

    add: mysql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,28 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from 'src/tasks/models/task.model';
-import { TasksModule } from 'src/tasks/tasks.module';
-import { UsersModule } from './users/users.module';
-import { User } from 'src/users/models/user.model';
-import { JwtModule } from '@nestjs/jwt';
-import 'dotenv/config';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Task } from "src/tasks/models/task.model";
+import { TasksModule } from "src/tasks/tasks.module";
+import { UsersModule } from "./users/users.module";
+import { User } from "src/users/models/user.model";
+import { JwtModule } from "@nestjs/jwt";
+import "dotenv/config";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'tasks.db',
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Task, User],
       synchronize: true,
     }),
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: "60s" },
     }),
     TasksModule,
     UsersModule,
